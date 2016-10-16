0893920f8c5251b385d69d54b1764bc2bc7dbe78
fcu-D0449005 <austria5533@gmail.com>
2016-10-16 19:50:50 +0800

    作業三 第一個作業

---
+++ src/fcu/iecs/oop/Break.java
@@ -0,0 +1,36 @@
+package fcu.iecs.oop;
+
+
+public class Break {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 for(int i=0;i<5;i++)
+		    {
+		    	switch(i)
+		    	{
+		    	case 1:
+		    		
+		    		System.out.println("0");	
+		    		break;
+		    	case 2:
+		    		
+		    		System.out.println("1");	
+		    		break;	
+		    	case 3:
+		    		
+		    		break;
+		    	case 4:
+		    		
+		    		break;
+		    	case 5:
+		    		
+		    		break;
+		    	}
+		    	
+		     }
+    	
+	}
+
+}
