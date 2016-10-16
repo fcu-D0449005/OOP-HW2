268bc12513364fe921f811d04cd6e496222c7db7
Ivy <Ivy@192.168.0.5>
2016-10-16 21:05:18 +0800

    作業三 第二個作業

---
+++ src/fcu/iecs/oop/Continue.java
@@ -0,0 +1,22 @@
+package fcu.iecs.oop;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 for(int i=0;i<5;i++)
+		    {
+		    
+		    	if(i==0)
+		    	    continue;
+		    	else if(i==2)
+		    		continue;
+		    	else if(i==4)
+	    	        continue;
+		    	System.out.println(i);	
+		    	
+		     }
+ 	
+	}
+
+}
