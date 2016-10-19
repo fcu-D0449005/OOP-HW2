5d3f87ef3a77fb6ed8afaa3abc4d3ef0fda15b55
fcu-D0449005 <austria5533@gmail.com>
2016-10-19 23:37:25 +0800

    更新  作業三第三個作業

--- src/fcu/iecs/oop/StringComparator.java
+++ src/fcu/iecs/oop/StringComparator.java
@@ -5,27 +5,26 @@ public class StringComparator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String n1,n2;
-
 		Scanner keyboard = new Scanner(System.in);
-		 
-		System.out.print(" enter a integer 1:");
+				String n1,n2;
+				int a=0; 
+		
+	   do
+		{ 
+
+	     System.out.print(" enter a integer 1:");
 		 n1 = keyboard.next();
 		 System.out.print(" enter a integer 2:");
 		 n2 = keyboard.next();
-		
-		 keyboard.close();
-		
+	     a=n1.compareToIgnoreCase(n2);
+	     if(a!=0)System.out.print("The two strings are not the same.\n");
 
-        if( n1.equalsIgnoreCase(n2))
-        {
+		}while(a!=0);
+        
+      System.out.print("The two strings are the same.");
         	
-          System.out.print("The two strings are the same.");
-        }
-        else
-        {
-        	System.out.print("The two strings are not the same.");
-        }
+ 	    keyboard.close();
+	 }
+	
 	}
 
-}
